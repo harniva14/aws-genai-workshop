e62cb8551338216284f183bf86a9d7e840af9724
EC2 Default User <ec2-user@ip-172-31-83-109.ec2.internal>
2023-07-29 23:47:33 +0000

    update setup

---
+++ 00_setup/install_aws_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 -m pip install ../dependencies/boto3-1.26.162-py3-none-any.whl --force-reinstall
+
+python3 -m pip install ../dependencies/botocore-1.29.162-py3-none-any.whl --force-reinstall
+
+#python3 -m pip install ../dependencies/awscli-1.27.162-py3-none-any.whl --force-reinstall
