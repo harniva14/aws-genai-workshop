819196ccd741e2c12bbc445ae5b6c855cee96404
EC2 Default User <ec2-user@ip-172-31-82-147.ec2.internal>
2023-07-30 02:17:28 +0000

    update image generation script

--- 03_image_generation/image_generation.py
+++ 03_image_generation/image_generation.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 import os
+import sys
 
 session = boto3.Session(profile_name='bedrock')
 boto3_bedrock = session.client('bedrock', 'us-east-1', endpoint_url='https://bedrock.us-east-1.amazonaws.com')
@@ -17,6 +18,9 @@ negative_prompts = [
     ]
 style_preset = "photographic" # (photographic, digital-art, cinematic, ...)
 
+module_path = "."
+sys.path.append(os.path.abspath(module_path))
+import bedrock
 model = bedrock.Bedrock(boto3_bedrock)
 base_64_img_str = model.generate_image(prompt, cfg_scale=5, seed=5450, steps=70, style_preset=style_preset)
 
